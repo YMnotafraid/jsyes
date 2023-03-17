094e768f329a97dab4c03e6960385d1dcf912ae5
YMnotafraid <867568537@qq.com>
2023-03-17 22:08:47 +0800

    feat:reduce

--- 数组/Array.reduce.js
+++ 数组/Array.reduce.js
@@ -1,14 +1,10 @@
 Array.prototype.myreduce = function (callback, ...args) {
-  let start = 0,
-    pre = 0;
-  if (args.length) {
-    pre = args[0];
-  }
-  for (let i = start; i < this.length; i++) {
+  let pre = args.length ? args[0] : 0;
+  for (let i = 0; i < this.length; i++) {
     pre = callback(pre, this[i], i, this);
   }
   return pre;
 };
 
 const arr = [1, 2, 3, 4];
-console.log(arr.myreduce((pre, cur) => pre + cur, 2, 3, 4));
+console.log(arr.myreduce((pre, cur) => pre + cur, 3, 3, 4));
