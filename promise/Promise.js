52455529c1951670bbd06c674c20871886da1e1b
YMnotafraid <867568537@qq.com>
2023-03-19 23:37:05 +0800

    promise/A+

--- promise/Promise.js
+++ promise/Promise.js
@@ -2,25 +2,27 @@ function Promise(executor) {
   this.state = "pending";
   this.value = undefined;
   this.reason = undefined;
+  // 保存成功回调
   this.onResolvedCallbacks = [];
+  // 保存失败回调
   this.onRejectedCallbacks = [];
 
   let _this = this;
-
   try {
     executor(resolve, reject);
   } catch (error) {
     reject(error);
   }
+
   function resolve(value) {
-    if ((_this.state = "pending")) {
+    if (_this.state === "pending") {
       _this.state = "resolved";
       _this.value = value;
       _this.onResolvedCallbacks.forEach((cb) => cb(value));
     }
   }
   function reject(reason) {
-    if ((_this.state = "pending")) {
+    if (_this.state === "pending") {
       _this.state = "rejected";
       _this.reason = reason;
       _this.onRejectedCallbacks.forEach((cb) => cb(reason));
@@ -29,7 +31,6 @@ function Promise(executor) {
 }
 
 Promise.prototype.then = function (onFulfilled, onRejected) {
-  //如果then中无回调，实现值传递
   onFulfilled =
     typeof onFulfilled === "function" ? onFulfilled : (value) => value;
   onRejected =
@@ -38,38 +39,39 @@ Promise.prototype.then = function (onFulfilled, onRejected) {
       : (err) => {
           throw err;
         };
+
   let promise2 = new Promise((resolve, reject) => {
-    //异步
+    // 等待态判断，此时异步代码还未走完，回调入数组队列
     if (this.state === "pending") {
       this.onResolvedCallbacks.push(() => {
         queueMicrotask(() => {
           try {
             let x = onFulfilled(this.value);
             resolvePromise(promise2, x, resolve, reject);
-          } catch (error) {
-            reject(error);
+          } catch (e) {
+            reject(e);
           }
         });
       });
+
       this.onRejectedCallbacks.push(() => {
         queueMicrotask(() => {
           try {
-            let x = onRejected(this.reason);
+            let x = onRejected(this.value);
             resolvePromise(promise2, x, resolve, reject);
-          } catch (error) {
-            reject(error);
+          } catch (e) {
+            reject(e);
           }
         });
       });
     }
-    //同步
     if (this.state === "resolved") {
       queueMicrotask(() => {
         try {
           let x = onFulfilled(this.value);
           resolvePromise(promise2, x, resolve, reject);
-        } catch (error) {
-          reject(error);
+        } catch (e) {
+          reject(e);
         }
       });
     }
@@ -78,29 +80,22 @@ Promise.prototype.then = function (onFulfilled, onRejected) {
         try {
           let x = onRejected(this.reason);
           resolvePromise(promise2, x, resolve, reject);
-        } catch (error) {
-          reject(error);
+        } catch (e) {
+          reject(e);
         }
       });
     }
   });
   return promise2;
 };
-/**
- * 解析then返回值与新Promise对象
- * @param {Object} 新的Promise对象，就是我们创建的promise2实例
- * @param {*} x 上一个then的返回值
- * @param {Function} resolve promise2处理器函数的resolve
- * @param {Function} reject promise2处理器函数的reject
- */
 
 function resolvePromise(promise2, x, resolve, reject) {
-  let called;
   if (promise2 === x) {
     reject(new TypeError("请避免Promise循环引用"));
   }
-  //返回值x是个对象或者函数
+  let called;
   if (x !== null && (typeof x === "object" || typeof x === "function")) {
+    // 可能是个对象或是函数
     try {
       let then = x.then;
       if (typeof then === "function") {
@@ -109,6 +104,7 @@ function resolvePromise(promise2, x, resolve, reject) {
           (y) => {
             if (called) return;
             called = true;
+            // 递归调用，传入y若是Promise对象，继续循环
             resolvePromise(promise2, y, resolve, reject);
           },
           (r) => {
@@ -120,49 +116,28 @@ function resolvePromise(promise2, x, resolve, reject) {
       } else {
         resolve(x);
       }
-    } catch (error) {
+    } catch (e) {
       if (called) return;
       called = true;
-      reject(error);
+      reject(e);
     }
   } else {
-    //x是普通值
+    // 普通值结束递归
     resolve(x);
   }
 }
 
-Promise.prototype.catch = function (reject) {
-  return this.then(null, reject);
-};
-
-// const p = new Promise((resolve, reject) => {
-//   resolve(1);
-// });
-// p.then((res) => {
-//   console.log(res);
-//   return new Promise((resolve, reject) => {
-//     resolve(
-//       new Promise((resolve, reject) => {
-//         reject(22);
-//       })
-//     );
-//   });
-// })
-//   .then(
-//     (res) => console.log(res)
-//     // (err) => console.log(err)
-//   )
-//   .catch((err) => console.log(err));
-
-// promises-aplus-tests测试
-Promise.defer = Promise.deferred = function () {
-  let defer = {};
-  defer.promise = new Promise((resolve, reject) => {
-    defer.resolve = resolve;
-    defer.reject = reject;
-  });
-  return defer;
-};
-try {
-  module.exports = Promise;
-} catch (e) {}
+new Promise((resolve) => {
+  resolve(1);
+})
+  .then((res) => {
+    console.log(res);
+    return new Promise((resolve) => {
+      resolve(
+        new Promise((resolve) => {
+          resolve(11);
+        })
+      );
+    });
+  })
+  .then((res) => console.log(res));
