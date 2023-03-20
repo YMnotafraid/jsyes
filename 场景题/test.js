82e1c1d86eff2ca379231a1d19b6fe429048a31d
YMnotafraid <867568537@qq.com>
2023-03-20 23:14:46 +0800

    ajax

--- 场景题/test.js
+++
@@ -1,8 +0,0 @@
-function SnapPea(name) {
-  const obj = Object.create(SnapPea.prototype);
-  obj.name = name;
-  return obj;
-}
-var tick = new SnapPea("tick");
-var tock = SnapPea("tock");
-console.log(tick instanceof SnapPea && tock instanceof SnapPea);
