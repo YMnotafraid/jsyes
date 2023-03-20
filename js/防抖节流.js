c464bf5db9b81919315bccfade991b8403886037
YMnotafraid <867568537@qq.com>
2023-03-20 20:03:12 +0800

    防抖节流

---
+++ js/防抖节流.js
@@ -0,0 +1,21 @@
+//防抖
+function debounce(fn, time) {
+  let timer = null;
+  return function (...args) {
+    if (timer) clearInterval(timer);
+    timer = setInterval(() => {
+      fn.apply(this, args);
+    }, time);
+  };
+}
+//节流
+function throttle(fn, time) {
+  let t1 = 0;
+  return function (...args) {
+    let t2 = new Date();
+    if (t2 - t1 > time) {
+      fn.apply(this, args);
+      t1 = t2;
+    }
+  };
+}
