d2e53dcb33975f1733fa99281d77fd61ef4a85d8
YMnotafraid <867568537@qq.com>
2023-03-25 23:36:43 +0800

    笔试

---
+++ 笔试题/打家劫舍变形.js
@@ -0,0 +1,13 @@
+const nums = [3, 1, 2, 7, 10, 2, 4];
+const dp = Array(nums.length).fill(0);
+let res = 0;
+dp[0] = nums[0];
+dp[1] = Math.max(nums[0], nums[1]);
+dp[2] = Math.max(dp[1], nums[2]);
+for (let i = 3; i < nums.length; i++) {
+  dp[i] = Math.max(dp[i - 1], dp[i - 3] + nums[i]);
+  console.log(dp[i]);
+  res = Math.max(res, dp[i]);
+  //   console.log(res);
+}
+console.log(res);
---
+++ 笔试题/火车进站.js
@@ -0,0 +1,19 @@
+const start = [1, 2, 3];
+const end = [3, 2, 1];
+function isValue() {
+  let i = 0,
+    j = 0;
+  const st = [];
+  while (j < end.length) {
+    while (!st.length || st[st.length - 1] !== end[j]) {
+      if (i < start.length) st.push(start[i++]);
+      else return false;
+    }
+    st.pop();
+    j++;
+  }
+  if (st.length === 0) return true;
+  return false;
+}
+
+console.log(isValue());
