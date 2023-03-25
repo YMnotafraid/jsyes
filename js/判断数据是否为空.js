788a2c26ec595e12b57206ce5c7bfd5ec4f5ce3c
YMnotafraid <867568537@qq.com>
2023-03-25 15:17:06 +0800

    判断数据是否为空

---
+++ js/判断数据是否为空.js
@@ -0,0 +1,20 @@
+function isEmpty(data) {
+  if (typeof data === "undefined" || data === null) return true;
+  else if (typeof data === "string" && data.trim() === "") return true;
+  else if (Array.isArray(data) && data.length === 0) return true;
+  else if (typeof data === "object" && Object.keys(data).length === 0)
+    return true;
+  else return false;
+}
+
+console.log(isEmpty(null)); // true
+console.log(isEmpty(undefined)); // true
+console.log(isEmpty("")); // true
+console.log(isEmpty([])); // true
+console.log(isEmpty({})); // true
+
+console.log(isEmpty(0)); // false
+console.log(isEmpty(false)); // false
+console.log(isEmpty("Hello")); // false
+console.log(isEmpty([1, 2, 3])); // false
+console.log(isEmpty({ a: 1, b: 2 })); // false
