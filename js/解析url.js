b555164c19841187422af1198c69b769945ff3be
YMnotafraid <867568537@qq.com>
2023-04-01 22:10:00 +0800

    解析url

--- js/解析url.js
+++ js/解析url.js
@@ -1,15 +1,14 @@
 const url =
   "https://www.baidu.com/s?wd=%E5%BE%AE%E4%BF%A1%E5%B0%8F%E7%A8%8B%E5%BA%8F%E5%BC%80%E9%A2%98%E6%8A%A5%E5%91%8A&rsv_spt=1&rsv_iqid=0xa3f5eb180001d272&issp=1&f=8&rsv_bp=1&rsv_idx=2&ie=utf-8&tn=baiduhome_pg&rsv_enter=1&rsv_dl=ib&rsv_sug3=2&rsv_n=2";
 //暴力api
-const [url1, url2] = url.split("?");
-const arr = url2.split("&");
+const [arr1, arr2] = url.split("?");
+const arr = arr2.split("&");
 let obj = {};
-for (const item of arr) {
-  let [key, value] = item.split("=");
+for (let item of arr) {
+  const [key, value] = item.split("=");
   obj[key] = value;
 }
 console.log(obj);
-
 //正则
 function transform(url) {
   const obj = {};
