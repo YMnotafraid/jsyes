441460a8d55c6c5ad13481c56e698b549a4c6da3
YMnotafraid <867568537@qq.com>
2023-04-15 15:17:01 +0800

    不改变原数组

--- 排序/快排.js
+++ 排序/快排.js
@@ -1,4 +1,5 @@
-function quickSort(arr) {
+function quickSort(data) {
+  const arr = [...data];
   if (arr.length < 2) return arr;
   let left = [],
     right = [];
@@ -9,18 +10,7 @@ function quickSort(arr) {
   return [...quickSort(left), cur, ...quickSort(right)];
 }
 
-function quick_sort(arr) {
-  if (arr.length < 2) return arr;
-  let left = [],
-    right = [];
-  let cur = arr.shift();
-  for (let num of arr) {
-    num > cur ? right.push(num) : left.push(num);
-  }
-  return [...quick_sort(left), cur, ...quick_sort(right)];
-}
-
 //test
 const arr = [98, 42, 25, 54, 15, 3, 25, 72, 41, 10, 121];
 console.log(quickSort(arr));
-console.log(quick_sort(arr));
+console.log(arr);
