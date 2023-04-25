b77c78bba9e7e81d33aa8f14d1d93fd090f6909c
YMnotafraid <867568537@qq.com>
2023-04-25 20:20:39 +0800

    await

---
+++ promise/红路灯.js
@@ -0,0 +1,23 @@
+function red() {
+  console.log("red");
+}
+function green() {
+  console.log("green");
+}
+function yellow() {
+  console.log("yellow");
+}
+function light(color, time) {
+  return new Promise((r) =>
+    setTimeout(() => {
+      r();
+      color();
+    }, time)
+  );
+}
+async function step() {
+  await light(red, 3000);
+  await light(green, 2000);
+  await light(yellow, 1000);
+}
+step();
