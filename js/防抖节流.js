3a178a380f8ba879be94d5ba70b5ed52904e0335
YMnotafraid <867568537@qq.com>
2023-06-03 20:34:02 +0800

    debounce

--- js/防抖节流.js
+++ js/防抖节流.js
@@ -2,13 +2,12 @@
 function debounce(fn, time) {
   let timer = null;
   return function (...args) {
-    if (timer) clearInterval(timer);
-    timer = setInterval(() => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
       fn.apply(this, args);
     }, time);
   };
 }
-
 //节流
 function throttle(fn, time) {
   let t1 = 0;
